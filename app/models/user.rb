8fddc8fef2a489238dede6eaf078e9ffc5cc69f2
Alex <ababich@asu.edu>
2015-04-26 18:26:27 -0700

    added provider key

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, :omniauth_providers => [:facebook, :twitter]
 
   def self.new_with_session(params, session)
     super.tap do |user|
