ed45ea9e3e2d57b5f2406ba5fed895ba4f5fa577
Alex <ababich@asu.edu>
2015-04-26 17:21:47 -0700

    Added name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426234946) do
+ActiveRecord::Schema.define(version: 20150427002104) do
 
   create_table "riddles", force: :cascade do |t|
     t.string   "question"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150426234946) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
