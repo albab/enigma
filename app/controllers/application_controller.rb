c448cfa3c909f0476d6328d5344306077d60b8ad
Alex <ababich@asu.edu>
2015-05-04 10:13:22 -0700

    fixed devise I think

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
-  def after_sign_in_path_for(resource_or_scope)
-   if request.env['omniauth.origin']
-      request.env['omniauth.origin']
-    end
+  def new_session_path(scope)
+    new_user_session_path
   end
 
 end
