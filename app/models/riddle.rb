eeb6020a80371bdc832511984ebbe88bcbf52db2
Alex <ababich@asu.edu>
2015-05-05 14:54:27 -0700

    asd

--- app/models/riddle.rb
+++ app/models/riddle.rb
@@ -1,5 +1,5 @@
 class Riddle < ActiveRecord::Base
   extend ::Geocoder::Model::ActiveRecord
-  geocoded_by :latitude, :longitude
+  geocoded_by :address, :latitude => :lat, :longitude => :lon
   after_validation :geocode
 end
